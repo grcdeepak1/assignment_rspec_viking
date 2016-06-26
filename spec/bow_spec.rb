911e2c54cf17d62a5507e325e67a39df1599b1c8
Deepak <grc.deepak@gmail.com>
2016-06-26 12:04:29 -0700

    tested Bow and added guard for automatic testing

--- spec/bow_spec.rb
+++ spec/bow_spec.rb
@@ -1,1 +1,23 @@
-# Your code here
+require_relative '../lib/weapons/bow.rb'
+describe Bow do
+  let(:bow) { Bow.new }
+
+  it "Bow's arrow count is readable" do
+    expect(bow.arrows).to be_a(Fixnum)
+  end
+  it "Bow starts with 10 arrows by default" do
+    expect(bow.arrows).to eq(10)
+  end
+  it "Bow created with a specified number of arrows starts with that number of arrows" do
+    bow_7 = Bow.new(7)
+    expect(bow_7.arrows).to eq(7)
+  end
+  it "useing a Bow reduces arrows by 1" do
+    bow.use
+    expect(bow.arrows).to eq(9)
+  end
+  it "useing a Bow with 0 arrows throws an error" do
+    bow_0 = Bow.new(0)
+    expect {bow_0.use}.to raise_error(RuntimeError)
+  end
+end
